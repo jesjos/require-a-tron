ee5c0323ebe6f5256d6dff1ceae0ed0907fa1211
Jesper Josefsson <jesper.josefsson@gmail.com>
2012-10-02 15:40:47 +0200

    Add author to requirement

--- app/models/requirement.rb
+++ app/models/requirement.rb
@@ -7,7 +7,8 @@ class Requirement < ActiveRecord::Base
   has_and_belongs_to_many :dependencies, class_name: "Requirement", association_foreign_key: :dependent_id, join_table: "dependencies"
   has_and_belongs_to_many :related_requirements, class_name: "Requirement", association_foreign_key: :related_requirement_id, join_table: "related_requirements"
   has_and_belongs_to_many :conflicting_requirements, class_name: "Requirement", association_foreign_key: :conflicting_requirement_id, join_table: "conflicting_requirements"
-
+  belongs_to :author, class_name: "User"
+  
   def relations
   	Relation.where("requirement_id = ? OR related_requirement_id = ?", self.id, self.id)
   end
---
+++ db/migrate/20121002133551_add_author_to_requirement.rb
@@ -0,0 +1,4 @@
+class AddAuthorToRequirement < ActiveRecord::Migration
+  def change
+  end
+end
