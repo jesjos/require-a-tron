cf53c5f524eee977d710f3ef8d10489e8b4dba3f
Jesper Josefsson <jesper.josefsson@gmail.com>
2012-10-14 14:11:25 +0200

    Add means of fetching a section

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -1,9 +1,13 @@
 class SectionsController < ApplicationController
+	respond_to :json, :text, :html
   def show
   	@section = Section.find(params[:id])
+  	@requirements = @section.requirements
+  	respond_with(@section)
   end
 
   def index
   	@sections = Section.all
+  	respond_with(@sections)
   end
 end
